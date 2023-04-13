c070f5cc2a1415e7d2e20beec1238013f6f98e51
Baked Pineapple <bakedpineapple@airmail.cc>
2023-04-13 14:41:16 -0700

    fix file loading for non-utf-8 characters on windows

--- sovits_utils.py
+++ sovits_utils.py
@@ -424,7 +424,8 @@ def load_wav_to_torch(full_path):
 
 
 def load_filepaths_and_text(filename, split="|"):
-  with open(filename, encoding='utf-8') as f: 
+  with open(filename, encoding=
+        'utf-8' if os.name == 'posix' else 'cp1252') as f: 
     filepaths_and_text = [line.strip().split(split) for line in f]
   return filepaths_and_text
 
